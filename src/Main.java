4c2f78b9927873d7d9bb1465f2fd98bf722ac955
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-21 10:45:32 +0530

    Update main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.stream.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IntStream
+                .range(1,10)
+
+                .forEach(System.out::println);
+        System.out.println();
     }
 }
