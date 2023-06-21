47df9a166292ac87fa7e40069b581a632675b791
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-21 14:49:45 +0530

    files and bands

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.*;
 
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(
                 IntStream
                         .range(1,11 )
@@ -43,11 +47,61 @@ public class Main {
                 .map(String::toLowerCase)
                 .filter(x->x.startsWith("d"))
                 .forEach(System.out::println);
-        //Reduction- sum
+//  Stream rows from text file, sort, filter, and print
+        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+        bands
+                .sorted()
+                .filter(x -> x.length() > 13)
+                .forEach(System.out::println);
+        bands.close();
+
+        // Stream rows from text file and save to List
+        List<String> bands2 = Files.lines(Paths.get("bands.txt"))
+                .filter(x -> x.contains("jit"))
+                .collect(Collectors.toList());
+        bands2.forEach(x -> System.out.println(x));
+
+        //  Stream rows from CSV file and count
+        Stream<String> rows1 = Files.lines(Paths.get("data.txt"));
+        int rowCount = (int)rows1
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .count();
+        System.out.println(rowCount + " rows.");
+        rows1.close();
+
+        // . Stream rows from CSV file, parse data from rows
+        Stream<String> rows2 = Files.lines(Paths.get("data.txt"));
+        rows2
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .forEach(x -> System.out.println(x[0] + "  " + x[1] + "  " + x[2]));
+        rows2.close();
+
+        //  Stream rows from CSV file, store fields in HashMap
+        Stream<String> rows3 = Files.lines(Paths.get("data.txt"));
+        Map<String, Integer> map = new HashMap<>();
+        map = rows3
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .collect(Collectors.toMap(
+                        x -> x[0],
+                        x -> Integer.parseInt(x[1])));
+        rows3.close();
+        for (String key : map.keySet()) {
+            System.out.println(key + "  " + map.get(key));
+        }
+
+        //Reduction-sum
 
         double total = Stream.of(7.3, 8.2, 10.0)
                 .reduce(0.0, (Double a, Double b)-> a+b);
         System.out.println(total);
 
+        IntSummaryStatistics summary = IntStream.of(7, 4,3,5,7,8,9,0,3)
+                .summaryStatistics();
+        System.out.println(summary);
     }
 }
